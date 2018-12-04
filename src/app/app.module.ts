d1b72e909cd342dc626fb78b2f933719ec2d6715
Xin Meng <x.meng@outlook.com>
2018-12-04 10:38:30 +0000

    Why: adjust the format of the file *
    This change addresses the need by:
    *
    
    [issue: X]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreModule } from "./store/store.module";
+import { StoreModule } from './store/store.module';
 @NgModule({
   declarations: [
     AppComponent
